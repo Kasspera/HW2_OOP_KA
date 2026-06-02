dddd1c421a078df4b9bedbfe78446306686a84a9
Каспер Аркадий <aakasper@edu.hse.ru>
2026-06-02 15:05:16 +0300

    класс Recipe

--- main.py
+++ main.py
@@ -29,3 +29,54 @@ class Ingredient:
 
     def __eq__(self, other):
         return self.name == other.name and self.unit == other.unit
+
+
+class Recipe:
+
+    def __init__(self, title, ingredients=None):
+        self.title = title
+        if ingredients is None:
+            self.ingredients = []
+        else:
+            self.ingredients = ingredients
+
+
+    def add_ingredient(self, ingredient):
+        for old_ingredient in self.ingredients:
+            if old_ingredient == ingredient:
+                old_ingredient.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        if type(ratio) == int or type(ratio) == float:
+            return ratio > 0
+        return False
+
+
+    def scale(self, ratio):
+        if not Recipe.is_valid_ratio(ratio):
+            raise ValueError("Коэффициент должен быть положительным")
+
+        new_ingredients = []
+        for ingredient in self.ingredients:
+            new_ingredient = Ingredient(
+                ingredient.name,
+                ingredient.quantity * ratio,
+                ingredient.unit
+            )
+            new_ingredients.append(new_ingredient)
+        return Recipe(self.title, new_ingredients)
+
+
+    def __len__(self):
+        return len(self.ingredients)
+
+
+    def __str__(self):
+        text = self.title + "\n"
+        for ingredient in self.ingredients:
+            text += str(ingredient) + "\n"
+        return text
